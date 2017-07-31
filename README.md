463ddf8008cee1a11412e691d79d24bb32aaa599
Alexander Yankovskiy-Betcher <a@y-b.one>
2017-07-31 21:02:44 +0300

    Updating Centos script and fixing errors

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # This is a repo for the instant deploy of the Grav flat CMS
 
+This repo is still in development, so don't appreciate too much, but you can try it at your own risk.
+
 ## Instructions:
 
+### Centos VPS
 1. Login to your fresh Centos VPS
 2. `wget https://raw.githubusercontent.com/ay-b/grav_default/master/rungrav.sh`
-3. sh rungrav.sh
+3. `sh rungrav.sh`
 4. ..
 5. Profit!
 
+### Debian/Ubuntu **STILL IN DEVELOPMENT**
+1. Login to your fresh Debian VPS
+2. `wget https://raw.githubusercontent.com/ay-b/grav_default/master/rungrav_debian.sh`
+3. `sudo sh rungrav.sh`
+4. ..
+5. Profit!
+
+## Troubleshooting
+
+Scroll up to error and send me an issue with it.
+Don't forget to tell your OS version and where are you hosting your VPS
--- rungrav.sh
+++ rungrav.sh
@@ -1,5 +1,13 @@
 #/bin/bash
-
+echo "
+====================================================
+||                     ATTENTION                  ||
+====================================================
+||  If the script exited with error, then check:  ||
+||  1. Internet connection. May be it's limited   ||
+||  2. Your /var/www folder must be empty         ||
+====================================================
+"
 yum update -y
 yum upgrade -y
 yum install epel-release -y
@@ -8,12 +16,23 @@ yum install docker docker-compose wget git unzip -y
 chkconfig docker on
 service docker start
 
-cd /root
-git clone https://github.com/ay-b/grav_default.git
-cd /root/grav_default/html
+mkdir -p /var/www
+cd /var/www
+git clone https://github.com/ay-b/grav_default.git .
+cd ./html
 wget https://getgrav.org/download/core/grav-admin/1.3.1 -O grav.zip
 unzip grav.zip
-mv grav-admin/* ./
+mv grav-admin/* ./ && rm -rf ./grav-admin 
 cd ..
 docker-compose up -d --build
 (crontab -l 2>/dev/null; echo "@reboot docker-compose up -f /root/grav_default/Docker-compose.yml") | crontab -
+
+echo "
+====================================================
+||               OKAY WE'RE DONE                  ||
+====================================================
+||  Now you can check your Grav web-site by IP    ||
+||  or domain name if you have one already here   ||
+||  Don't launch the script again, please         ||
+====================================================
+"
