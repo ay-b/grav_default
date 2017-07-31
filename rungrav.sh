b66231af7d3165a4c3f3d8cbaa2158f4a7fd5a7e
Alexander Yankovskiy-Betcher <a@y-b.one>
2017-07-31 16:28:25 +0300

    Initial commit

--- rungrav.sh
+++ rungrav.sh
@@ -8,9 +8,11 @@ yum install docker docker-compose wget git -y
 chkconfig docker on
 service docker start
 
-wget https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/ay-b/docker/tree/master/grav -O rungrav.zip
-unzip rungrav.zip 
-cd rungrav
+git clone https://github.com/ay-b/grav_default.git
+cd grav_default/html
 wget https://getgrav.org/download/core/grav-admin/1.3.1 -O grav.zip
 unzip grav.zip
-mv grav-admin/* html/
+mv grav-admin/* ./
+cd ..
+docker-compose up -d --build
+(crontab -l 2>/dev/null; echo "@reboot docker-compose up -f /root/Docker-compose.yml") | crontab -
